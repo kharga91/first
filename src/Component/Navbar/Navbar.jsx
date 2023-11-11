040a16a7c0d13b02799dced036a12c12ef18edcc
Aniket Kharga <aniketkharga91@gmail.com>
2023-11-11 09:25:27 +0545

    change

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Navbar.css'
+import Logo from '../../img/logo.png.png'
+const navbar = () => {
+  return (
+   <div className="n-wrapper">
+    <div className="n-left">
+    <div className="n-logo">
+        <img className='logo' src={Logo} alt="" />
+        </div>
+    <span>Toggle</span>
+      
+    </div>
+    <div className="n-right">
+      <div className="n-list">
+        <ul style={{listStyleType:'none'}}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Education</li>
+          <li>Services</li>
+          <li>Experience</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <button className="button n-button">DOWNLOAD 
+      CV</button>
+    </div>
+   </div>
+  )
+}
+
+export default navbar
+
